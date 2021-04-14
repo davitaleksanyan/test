b97c9e0dc6669c2f343a904b14e5b92a455e2e06
davitaleksanyan <81590061+davitaleksanyan@users.noreply.github.com>
2021-04-14 22:58:44 +0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+#Speed of a vehicle
+time = 60 # time in hour
+distance = 25 # distance in km
+speed = distance / time
+print("Vehincle speed is " + str(speed) + " km/h")
+# Simple interest formula
+p = 1000 # the initial amount of the deposit (loan)
+i = 0.24 # simple interest rate
+T = 730 # loan term in days
+t = 365 # numbers of days in year
+n = T/t
+S = p * (1 + i * n)
+print(S)
+# Area of Circle
+import math
+pi = math.pi
+radius = input("Enter radius: " )
+S = pi * int(radius)**2
+print("Area of Circle is " + str(S))
+# 4=th ex.
+a = 5
+b = 6
+print((a + b)**2)
+# 5th
+a = 1
+b = 2
+print(a)
+print(b)
+b = a
+print("After assign b is: " + str(b))
